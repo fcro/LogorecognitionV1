4d4fc08e872b77c864871cabbd3c4f42a06a1bb8
fcro <floriancrosnier@hacari.net>
2016-11-22 11:22:37 +0100

    Renommage et refactor de CVManager

--- app/src/main/java/com/telecom/cottoncrosnier/logorecognition/image/MatManager.java
+++ app/src/main/java/com/telecom/cottoncrosnier/logorecognition/image/MatManager.java
@@ -1,6 +1,5 @@
 package com.telecom.cottoncrosnier.logorecognition.image;
 
-import android.net.Uri;
 import android.util.Log;
 
 import org.bytedeco.javacpp.Loader;
@@ -8,22 +7,18 @@ import org.bytedeco.javacpp.opencv_calib3d;
 import org.bytedeco.javacpp.opencv_core.*;
 import org.bytedeco.javacpp.opencv_nonfree.*;
 
-import java.io.File;
-
 import static org.bytedeco.javacpp.opencv_core.NORM_L2;
 import static org.bytedeco.javacpp.opencv_features2d.*;
 import static org.bytedeco.javacpp.opencv_highgui.*;
 
 /**
- * Cette classe sert juste à implémenter quelques appels de JavaCV pour l'instant
- * Il faudra la refactorer suivant comment on veut l'appeler, stocker les résultats, etc. (limite la scinder en plusieurs classes)
- *
  * Created by fcro on 16/11/2016.
  */
 
-public class CVManager {
+public class MatManager {
 
-    private static final String TAG = CVManager.class.getSimpleName();
+    private static final String TAG = MatManager.class.getSimpleName();
+
     private static final int N_FEATURES = 0; // nbr meilleures caracteristiques a retenir
     private static final int N_OCTAVE_LAYERS = 3; // nbr couches dans chaque octave
     private static final double CONTRAST_THRESHOLD = 0.03; // seuil de contraste pour filtrer les caracteristiques faibles en regions semi-uniformes
@@ -31,44 +26,47 @@ public class CVManager {
     private static final double SIGMA = 1.6; // sigma de la gaussienne (reduire si l'image est de faible resolution)
 
     private Mat mMat;
-    private Mat mDescriptor;
     private SIFT mSift;
-    private KeyPoint mKeyPoints;
-    private DMatch mMatches;
 
 
-    public CVManager(String path) {
-
-        Log.d(TAG, "CVManager() called with: path = [" + path + "]");
+    public MatManager(String path) {
+        Log.d(TAG, "MatManager() called with: path = [" + path + "]");
         this.mMat = imread(path);
         this.mSift = new SIFT(N_FEATURES, N_OCTAVE_LAYERS, CONTRAST_THRESHOLD, EDGE_THRESHOLD, SIGMA);
-        this.mKeyPoints = null;
-        this.mDescriptor = null;
-        this.mMatches = null;
     }
 
 
-    public Mat getMat() {
+    private Mat getMat() {
         return mMat;
     }
 
-    public KeyPoint getKeypoints() {
+    private KeyPoint getKeypoints(Mat mat) {
+        KeyPoint keypoints = new KeyPoint();
+
         Loader.load(opencv_calib3d.class);
-        mSift.detect(mMat, mKeyPoints);
+        mSift.detect(mat, keypoints);
 
-        return mKeyPoints;
+        return keypoints;
     }
 
-    public Mat getDescriptor() {
-        mSift.compute(mMat, mKeyPoints, mDescriptor);
+    private Mat getDescriptor(Mat mat, KeyPoint keypoints) {
+        Mat descriptor = new Mat();
 
-        return mDescriptor;
+        mSift.compute(mat, keypoints, descriptor);
+
+        return descriptor;
     }
 
-    public DMatch getMatchesWith(Mat descriptor) {
+    public DMatch getMatchesWith(MatManager otherMat) {
+        DMatch matches = new DMatch();
+
+        Mat descriptor = getDescriptor(getMat(), getKeypoints(getMat()));
+        Mat otherDescriptor =
+                otherMat.getDescriptor(otherMat.getMat(), otherMat.getKeypoints(otherMat.getMat()));
+
         BFMatcher matcher = new BFMatcher(NORM_L2, false);
-        matcher.match(mDescriptor, descriptor, mMatches);
+        matcher.match(descriptor, otherDescriptor, matches);
 
-        return mMatches;
+        return matches;
     }
 }
